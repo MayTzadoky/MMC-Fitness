a70053a194a20f6a802a372f77907528d6e6dd0b
May Tzadoki <mayta@mta.ac.il>
2022-04-26 21:32:18 +0300

    added java project

---
+++ MMC-Fitness/Logic/src/Trainer.java
@@ -0,0 +1,9 @@
+public class Trainer extends Subscriber{
+    private enum specialty{};
+    private enum seniority{};
+
+    public Trainer() {
+    }
+
+
+}
---
+++ MMC-Fitness/Logic/src/User.java
@@ -0,0 +1,60 @@
+import java.util.Objects;
+
+public class User extends Subscriber{
+    private String firstName;
+    private String lastName;
+    private String phoneNumber;
+    private String email;
+
+    public User(String firstName, String lastName, String phoneNumber, String email) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.phoneNumber = phoneNumber;
+        this.email = email;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        User user = (User) o;
+        return firstName.equals(user.firstName) && lastName.equals(user.lastName) && phoneNumber.equals(user.phoneNumber) && email.equals(user.email);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(firstName, lastName, phoneNumber, email);
+    }
+}
