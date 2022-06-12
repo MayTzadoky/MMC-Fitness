a7d7241bd6024062460acfad8a171bfb307096ff
movadia <may.ovadia@cognyte.com>
2022-06-12 12:33:15 +0300

    fix use module

--- MMC-Fitness/Logic/src/Nutritionist.java
+++
@@ -1,7 +0,0 @@
-public class Nutritionist extends Subscriber{
-    private enum seniority{};
-
-    public Nutritionist(Integer userId, Integer age, Float height, Float weight, Integer workoutAmount, Float targetFatPercentage, Float targetWeight) {
-        super(userId, age, height, weight, workoutAmount, targetFatPercentage, targetWeight);
-    }
-}
